63ed51ed46db713fd8b78f153a17dcfc27cebd2d
zhangsan <zhangsan@itcast.cn>
2020-10-03 09:36:44 +0800

    第san次提交工程到git

---
+++ git-test-project/src/com/itheima/git/Frist.java
@@ -0,0 +1,20 @@
+package com.itheima.git;
+
+public class Frist {
+    public static void main(String[] args) {
+        System.out.println("hello project");
+        System.out.println("hello project");
+        System.out.println("hello project");
+        System.out.println("hello project");
+        System.out.println("hello project");
+        System.out.println("hello project");
+        System.out.println("hello project");
+    }
+
+    public void test(){
+        int i=1;
+        i+=10;
+        int a=100;
+    }
+
+}
